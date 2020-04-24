40192a808e08a6fa6423020140ee3b54a59ed7a2
guillecds <64273004+guillecds@users.noreply.github.com>
2020-04-24 18:22:27 -0300

    Add files via upload

---
+++ failure.php
@@ -0,0 +1,9 @@
+<div class="container m-t-20">
+    <div class="container">
+		  <div class="jumbotron text-center">
+			  <h1 class="display-3">UPS!</h1>
+			  <p class="lead"><strong>Pago rechazado</strong> por favor intente nuevamente.</p>
+			  <hr>
+		</div>
+    </div>
+</div>
---
+++ success.php
@@ -0,0 +1,9 @@
+<div class="container m-t-20">
+    <div class="container">
+		  <div class="jumbotron text-center">
+			  <h1 class="display-3">Gracias por su compra!</h1>
+			  <p class="lead"><strong>Pago aprobado</strong> nuestro equipo de postventa revisará el pago y cuando sea acreditado le enviaremos el comprobante de su compra y detalles sobre la entrega de sus articulos.</p>
+			  <hr>
+		</div>
+    </div>
+</div>
