da84f92744b4ab41bbc293d87db7f083aef362af
guillecds <64273004+guillecds@users.noreply.github.com>
2020-04-24 19:28:06 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -20,33 +20,41 @@ $item->unit_price = $_POST['price'];
 $preference->items = array($item);
 
 $preference->payer = array(
-   "name" => 'Lalo',
-   "surname" => 'Landa',
-   "email" => 'test_user_63274575@testuser.com',
+   "name" => "Lalo",
+   "surname" => "Landa",
+   "email" => "test_user_63274575@testuser.com",
    "phone" => array(
-    "area_code" => '011',
-    "number" => '2222-3333'
+    "area_code" => "011",
+    "number" => "2222-3333"
    ),
    "identification" => array(
-    "type" => 'DNI',
-    "number" => '22.333.444'
+    "type" => "DNI",
+    "number" => "22.333.444"
    ),
    "address" => array(
-    "street_name" => 'Falsa',
-    "street_number" => '123',
-    "zip_code" => '1111'
+    "street_name" => "Falsa",
+    "street_number" => "123",
+    "zip_code" => "1111"
    )
 );
 
 $preference->payment_methods = array(
   "excluded_payment_methods" => array(
-    "id" => 'amex'
+    "id" => "amex"
   ),
   "excluded_payment_types" => array(
-    "id" => 'atm'
+    "id" => "atm"
   ),
-  "installments" => '6'
+  "installments" => 6
 );
+
+$preference->back_urls = array(
+    "success"=> "https://guillecds-mp-commerce-php.herokuapp.com/success",
+    "pending"=> "https://guillecds-mp-commerce-php.herokuapp.com/pending",
+    "failure"=> "https://guillecds-mp-commerce-php.herokuapp.com/failure"
+);
+
+$preference->external_reference ="ABC1234";
 
 $preference->save();
 ?>
