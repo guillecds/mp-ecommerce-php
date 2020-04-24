1339673d7de73a30fc336aca1976c44b03c5f2d9
guillecds <64273004+guillecds@users.noreply.github.com>
2020-04-24 19:37:45 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -20,6 +20,43 @@ $item->unit_price = $_POST['price'];
 $preference->items = array($item);
 
 
+$preference->payer = array(
+   "name" => "Lalo",
+   "surname" => "Landa",
+   "email" => "test_user_63274575@testuser.com",
+   "phone" => array(
+    "area_code" => "011",
+    "number" => "2222-3333"
+   ),
+   "identification" => array(
+    "type" => "DNI",
+    "number" => "22.333.444"
+   ),
+   "address" => array(
+    "street_name" => "Falsa",
+    "street_number" => "123",
+    "zip_code" => "1111"
+   )
+);
+
+$preference->payment_methods = array(
+  "excluded_payment_methods" => array(
+    array("id" => "amex")
+  ),
+  "excluded_payment_types" => array(
+    array("id" => "atm")
+  ),
+  "installments" => 6
+);
+
+$preference->back_urls = array(
+    "approved"=> "https://guillecds-mp-commerce-php.herokuapp.com/success",
+    "pending"=> "https://guillecds-mp-commerce-php.herokuapp.com/pending",
+    "failure"=> "https://guillecds-mp-commerce-php.herokuapp.com/failure"
+);
+
+$preference->external_reference ="ABC1234";
+
 $preference->save();
 ?>
 
@@ -159,7 +196,8 @@ $preference->save();
                                       <script
                                        src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
                                        data-preference-id="<?php echo $preference->id; ?>"
-                                       data-elements-color="#2D3277">
+                                       data-elements-color="#2D3277"
+                                       data-button-label="Pagar la compra">
                                       </script>
                                     </form>
                                 </div>
