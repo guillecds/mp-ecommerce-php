91e26d02ae4ebdfbbfe3df1b075e4d530db15377
guillecds <64273004+guillecds@users.noreply.github.com>
2020-05-14 10:17:02 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -57,7 +57,7 @@ $preference->payment_methods = array(
 );
 
 $preference->back_urls = array(
-  "success" => "https://guillecds-mp-commerce.herokuapp.com/success",
+  "success" => "https://guillecds-mp-commerce.herokuapp.com/success.php",
   "failure" => "https://guillecds-mp-commerce.herokuapp.com/failure.php",
   "pending" => "https://guillecds-mp-commerce.herokuapp.com/pending.php"
 );
