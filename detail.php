36e25e266afbcf0e2617a8542e5f42d617df0850
guillecds <64273004+guillecds@users.noreply.github.com>
2020-05-14 10:30:58 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -205,7 +205,7 @@ print_r($preference);
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <form action="/procesar-pago" method="POST">
+                                    <form action="/procesar-pago.php" method="POST">
                                       <script
                                        src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
                                        data-preference-id="<?php echo $preference->id; ?>"
